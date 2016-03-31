5427a02d92930653cc084ce1449906f90eb225dc
Chriz <info@henschel-robotics.ch>
2016-03-31 21:22:38 +0200

    cleanup

--- WebGLGraph.js
+++ WebGLGraph.js
@@ -1,28 +1,23 @@
 
 var WebGLGraphs = [];
-var CANVAS_WIDTH;
-var CANVAS_HEIGHT;
-
-animate();
 
 function initWebGlGraph(graphContainer) {
 
-    var containers_main = graphContainer.split(',');
+    var containersMain = graphContainer.split(',');
 
-    for (var ii = 0; ii < containers_main.length; ++ii) {
-        var containers = containers_main[ii].split(':');
+    for (var ii = 0; ii < containersMain.length; ++ii) {
+        var containers = containersMain[ii].split(':');
         var graphs = containers[1];
 
         var container = document.getElementById(containers[0].trim());
-        document.body.appendChild(container);
-        var style = window.getComputedStyle(container),
-        CANVAS_WIDTH = style.getPropertyValue('width').replace("px", '');
-        CANVAS_HEIGHT = style.getPropertyValue('height').replace("px", '');
-        var graphTitle = style.getPropertyValue('name');
+        // document.body.appendChild(container);
 
-        graphTitle = container.getAttribute('name');
+        var style = window.getComputedStyle(container);
+        var canvasWidth = style.getPropertyValue('width').replace("px", '');
+        var canvasHeight = style.getPropertyValue('height').replace("px", '');
+        var graphTitle = container.getAttribute('class');
 
-        WebGLGraphs.push(new CWebGLGraphs(graphs, container, CANVAS_WIDTH, CANVAS_HEIGHT, graphTitle));
+        WebGLGraphs.push(new CWebGLGraphs(graphs, container, canvasWidth, canvasHeight, graphTitle));
     }
 }
 
@@ -32,23 +27,73 @@ function animate() {
     requestAnimationFrame(animate);
 }
 
-function CWebGLGraphs(functionCount, container, CANVAS_WIDTH, CANVAS_HEIGHT, graphTitle) {
+function CWebGLGraphs(functionCount, container, canvasWidth, canvasHeight, graphTitle) {
     var functionPoints = [], functionPointsTime = [];
+    var particlecount = canvasWidth;
 
-    var GraphTitle = graphTitle;
-    var PARTICLECOUNT = CANVAS_WIDTH * 6.5;
-
-    var Text2D = THREE_Text.Text2D;
+    var text2D = THREE_Text.Text2D;
     var textAlign = THREE_Text.textAlign;
 
     var xText = [], yText = [], colors = [];
 
-    var max_y, min_y, max_time;
+    var maxY, minY, maxTime;
     var camera, scene, renderer, particles = [];
 
     var hide = [];
 
     initGraph(functionCount);
+    
+    function initGraph(functionCount) {
+
+        functionPoints = createArray(functionCount);
+
+        //define the Y height of the graph
+        minY = -1000;
+        maxY = 1000;
+        maxTime = canvasWidth * 7;
+
+        camera = new THREE.PerspectiveCamera(40, window.innerWidth / window.innerHeight, 0.1, 20000);
+
+        camera.position.set(-3 * canvasWidth, 0, 3000);
+        camera.up = new THREE.Vector3(0, 1, 0);
+        camera.lookAt(-3 * canvasWidth, 0, 0);
+
+        scene = new THREE.Scene();
+        light = new THREE.AmbientLight(0xFFFFFF);
+        scene.add(light);
+
+        for (var qq = 0; qq < functionCount; ++qq) {
+            particles[qq] = new THREE.Geometry();
+            hide[qq] = 0;
+            colors = [];
+
+            for (ii = 0; ii < particlecount + 10 ; ii++) {
+                particles[qq].vertices.push(new THREE.Vector3());
+                colors[ii] = new THREE.Color(0xffffff);
+            }
+
+            particles[qq].colors = colors;
+            var material = new THREE.PointsMaterial({ size: 20, vertexColors: THREE.VertexColors });
+            scene.add(new THREE.Points(particles[qq], material));
+            scene.add(new THREE.Line(particles[qq], material));
+        }
+
+        renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+        renderer.setClearColor(0xffffff, 1);
+
+        document.body.appendChild(container);
+
+        camera.aspect = (canvasWidth / canvasHeight);
+        camera.updateProjectionMatrix();
+        renderer.setSize(canvasWidth, canvasHeight);
+        container.appendChild(renderer.domElement);
+
+        coordinateSystem();
+        drawBorder();
+        drawLines();
+        drawText();
+        animate();
+    }
 
     this.addPoint = function addPoint(x, y) {
         functionPointsTime.push(x);
@@ -60,6 +105,14 @@ function CWebGLGraphs(functionCount, container, CANVAS_WIDTH, CANVAS_HEIGHT, gra
             functionPoints[0].push(y);
     };
 
+    this.hideGraph = function hideGraph(ii) {
+        hide[ii] = !hide[ii];
+    };
+
+    this.animate = function () {
+        render();
+    };
+
     function createArray(length) {
         var myArr = new Array();
         for (ii = 0; ii < length; ++ii)
@@ -67,44 +120,41 @@ function CWebGLGraphs(functionCount, container, CANVAS_WIDTH, CANVAS_HEIGHT, gra
         return myArr;
     }
 
-    function CoordinateSystem() {
+    function coordinateSystem() {
         var geometry = new THREE.Geometry();
-
-        var vertex = new THREE.Vector3(0, max_y, 0); geometry.vertices.push(vertex);
-        var vertex2 = new THREE.Vector3(-0, min_y, 0); geometry.vertices.push(vertex2);
+        var vertex = new THREE.Vector3(0, maxY, 0); geometry.vertices.push(vertex);
+        var vertex2 = new THREE.Vector3(-0, minY, 0); geometry.vertices.push(vertex2);
         var vertex3 = new THREE.Vector3(-0, 0, 0); geometry.vertices.push(vertex3);
-        line = new THREE.Line(geometry, new THREE.LineBasicMaterial({ color: 0x00ff00, opacity: 1.0 }));
-        geometry = new THREE.Geometry();
+        var line = new THREE.Line(geometry, new THREE.LineBasicMaterial({ color: 0x00ff00, opacity: 1.0 }));
         scene.add(line);
 
+        geometry = new THREE.Geometry();
         vertex = new THREE.Vector3(0, 1000, 0); geometry.vertices.push(vertex);
         vertex2 = new THREE.Vector3(-0, -1000, 0); geometry.vertices.push(vertex2);
         vertex3 = new THREE.Vector3(-0, 0, 0); geometry.vertices.push(vertex3);
         line = new THREE.Line(geometry, new THREE.LineBasicMaterial({ color: 0x00ff00, opacity: 1.0 }));
-        geometry = new THREE.Geometry();
         scene.add(line);
     }
-
-
-    function DrawLines() {
+    
+    function drawLines() {
         var geometry = new THREE.Geometry();
         var vertex;
         var line;
-
-        vertex = new THREE.Vector3(0, 0, 0); geometry.vertices.push(vertex);
-        vertex = new THREE.Vector3(-max_time, 0, 0); geometry.vertices.push(vertex);
+        vertex = new THREE.Vector3(0, 0, 0);
+        geometry.vertices.push(vertex);
+        vertex = new THREE.Vector3(-maxTime, 0, 0);
+        geometry.vertices.push(vertex);
         line = new THREE.Line(geometry, new THREE.LineBasicMaterial({ linewidth: 1.0, color: 0x0000ff, opacity: 0.2, transparent: true }));
         scene.add(line);
     }
-
-
-    function DrawBorder() {
+    
+    function drawBorder() {
         var rectShape = new THREE.Shape();
         rectShape.moveTo(0, 0);
-        rectShape.lineTo(0, max_y);
-        rectShape.lineTo(-max_time, max_y);
-        rectShape.lineTo(-max_time, min_y);
-        rectShape.lineTo(0, min_y);
+        rectShape.lineTo(0, maxY);
+        rectShape.lineTo(-maxTime, maxY);
+        rectShape.lineTo(-maxTime, minY);
+        rectShape.lineTo(0, minY);
         rectShape.lineTo(0, 0);
 
         var rectGeom = new THREE.ShapeGeometry(rectShape);
@@ -112,70 +162,13 @@ function CWebGLGraphs(functionCount, container, CANVAS_WIDTH, CANVAS_HEIGHT, gra
         scene.add(rectMesh);
     }
 
-    function initGraph(functionCount) {
-
-        functionPoints = createArray(functionCount);
-
-        min_y = -1000;
-        max_y = 1000;
-        max_time = PARTICLECOUNT;
-
-        camera = new THREE.PerspectiveCamera(40, window.innerWidth / window.innerHeight, 0.1, 20000);
-
-        camera.position.set(-3 * CANVAS_WIDTH, 0, 3000);
-        camera.up = new THREE.Vector3(0, 1, 0);
-        camera.lookAt(-3 * CANVAS_WIDTH, 0, 0);
-
-        scene = new THREE.Scene();
-        light = new THREE.AmbientLight(0xFFFFFF);
-        scene.add(light);
-
-        for (var qq = 0; qq < functionCount; ++qq) {
-            particles[qq] = new THREE.Geometry();
-            hide[qq] = 0;
-            colors = [];
-
-            for (ii = 0; ii < PARTICLECOUNT * 2 ; ii++) {
-                particles[qq].vertices.push(new THREE.Vector3());
-                colors[ii] = new THREE.Color(0xffffff);
-            }
-
-            particles[qq].colors = colors;
-            material = new THREE.PointsMaterial({ size: 20, vertexColors: THREE.VertexColors });
-            scene.add(new THREE.Points(particles[qq], material));
-            scene.add(new THREE.Line(particles[qq], material));
-        }
-
-        renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
-        renderer.setClearColor(0xffffff, 1);
-
-        document.body.appendChild(container);
-
-
-        camera.aspect = (CANVAS_WIDTH / CANVAS_HEIGHT);
-        camera.updateProjectionMatrix();
-        renderer.setSize(CANVAS_WIDTH, CANVAS_HEIGHT);
-        container.appendChild(renderer.domElement);
-
-        CoordinateSystem();
-        DrawBorder();
-        DrawLines();
-        DrawText();
-        animate();
-    }
-
-    this.hideGraph = function hideGraph(ii) {
-        hide[ii] = !hide[ii];
-    };
-
-
-    function DrawText() {
+    function drawText() {
         var tempText;
         //draw X Axis
         for (var ii = 0; ii < 10; ++ii) {
-            tempText = new Text2D(10 - ii, { align: textAlign.center, font: '50px Arial', fillStyle: '#000000', antialias: true });
+            tempText = new text2D(10 - ii, { align: textAlign.center, font: '50px Arial', fillStyle: '#000000', antialias: true });
             tempText.material.alphaTest = 0.1;
-            tempText.position.set(ii * -PARTICLECOUNT / 10, -100, 10);
+            tempText.position.set(ii * -canvasWidth * 7 / 10, -100, 10);
             tempText.scale.set(1.5, 1.5, 1.5);
             tempText.rotation.set(0, 0, -1.0);
             xText.push(tempText);
@@ -184,7 +177,7 @@ function CWebGLGraphs(functionCount, container, CANVAS_WIDTH, CANVAS_HEIGHT, gra
 
         //draw Y Axis
         for (var ii = 0; ii < 9; ++ii) {
-            tempText = new Text2D(1000 - ii * 100, { align: textAlign.center, font: '50px Arial', fillStyle: '#000000', antialias: true });
+            tempText = new text2D(1000 - ii * 100, { align: textAlign.center, font: '50px Arial', fillStyle: '#000000', antialias: true });
             tempText.material.alphaTest = 0.1;
             tempText.position.set(150, 1000 + ii * -250, 10);
             tempText.scale.set(1.5, 1.5, 1.5);
@@ -192,21 +185,18 @@ function CWebGLGraphs(functionCount, container, CANVAS_WIDTH, CANVAS_HEIGHT, gra
             scene.add(tempText);
         }
 
-
         //draw Title
-        tempText = new Text2D(GraphTitle, { align: textAlign.center, font: '50px Arial', fillStyle: '#000000', antialias: true });
+        tempText = new text2D(graphTitle, { align: textAlign.center, font: '50px Arial', fillStyle: '#000000', antialias: true });
         tempText.material.alphaTest = 0.1;
-        tempText.position.set(-PARTICLECOUNT * 0.9, 1000, 10);
+        tempText.position.set(-particlecount * 0.9, 1000, 10);
         tempText.scale.set(1.5, 1.5, 1.5);
         scene.add(tempText);
-
-        return;
     }
 
     function renderGraph() {
         var colorTarget = 1.0;
-        var totalRange = max_y - min_y;
-        var zoom = 2000.0 / (max_y - min_y);
+        var totalRange = maxY - minY;
+        var zoom = 2000.0 / (maxY - minY);
 
         for (var pp = 0; pp < functionPoints.length; ++pp) {
             for (var ii = 0; ii < functionPoints[pp].length; ++ii) {
@@ -217,7 +207,7 @@ function CWebGLGraphs(functionCount, container, CANVAS_WIDTH, CANVAS_HEIGHT, gra
                 }
                 else {
                     colorTarget = 0.0;
-                    particles[pp].vertices[ii].y = ((functionPoints[pp][ii] - min_y) * zoom - 1000);
+                    particles[pp].vertices[ii].y = ((functionPoints[pp][ii] - minY) * zoom - 1000);
                 }
 
                 //move point along x axis
@@ -242,15 +232,15 @@ function CWebGLGraphs(functionCount, container, CANVAS_WIDTH, CANVAS_HEIGHT, gra
         }
 
         for (var ii = 8; ii > -1; ii--)
-            yText[ii].text = ((min_y + totalRange / 8 * (8 - ii)) / 10.0).toFixed(1);
+            yText[ii].text = ((minY + totalRange / 8 * (8 - ii)) / 10.0).toFixed(1);
         for (var ii = 0; ii < 10; ++ii)
             xText[ii].text = Math.round(functionPointsTime[functionPointsTime.length - 1] - 10 * ii, 2);
 
 
-        max_y = Math.max.apply(Math, functionPoints[0]);
-        min_y = Math.min.apply(Math, functionPoints[0]);
+        maxY = Math.max.apply(Math, functionPoints[0]);
+        minY = Math.min.apply(Math, functionPoints[0]);
 
-        if (functionPoints[0].length > PARTICLECOUNT) {
+        if (functionPoints[0].length > (particlecount*0.78)) {
             particles[0].colors.shift();
             particles[0].colors.push(new THREE.Color(0xff0000));
 
@@ -264,12 +254,7 @@ function CWebGLGraphs(functionCount, container, CANVAS_WIDTH, CANVAS_HEIGHT, gra
             functionPointsTime.shift();
         }
     }
-
-
-    this.animate = function () {
-        render();
-    };
-
+    
     function render() {
         renderGraph();
         renderer.render(scene, camera);
